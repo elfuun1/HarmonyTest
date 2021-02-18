c326ed0ee8d089389ae18b081722ad8f5020d6ee
Kenny <jessetuan@comcast.net>
2021-02-17 20:04:00 -0800

    Added Flower Dancing Code

---
+++ FlowerDancing/EventPatched.cs
@@ -0,0 +1,142 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerDancing
+{
+    public class EventPatched
+    {
+		public static IMonitor Monitor;
+		private static bool isMalePosition;
+
+		public static void Initialize(IMonitor monitor)
+		{
+			Monitor = monitor;
+		}
+
+		public static void setUpFestivalMainEvent_Kelly(StardewValley.Event __instance)
+		{
+			if(Monitor is null || __instance is null)
+			{
+				return;
+			}
+
+			else if(Game1.dayOfMonth != 24 && Game1.currentSeason != "spring")
+			{
+				return;
+			}
+
+			try
+			{
+				isMalePosition = true;
+				string[] eventCommands = __instance.eventCommands;
+
+				for(int i = 0; i < eventCommands.Length; i++)
+				{
+					string curr = eventCommands[i];
+
+					if (!curr.Contains("farmer"))
+					{
+						continue;
+					}
+
+					curr = fixFarmerDance(curr);
+					eventCommands[i] = curr;
+				}
+
+				if(!isMalePosition) // Make the farmer (if they are in the female position) do a final pose with their arms in the air
+				{
+					string farmerFinalPose = "showFrame 96";
+					string[] newEventCommands = new string[eventCommands.Length + 1];
+
+					for (int i = 0; i < newEventCommands.Length; i++)
+					{
+						if (i < 291)
+						{
+							newEventCommands[i] = eventCommands[i];
+						}
+
+						else if (i == 291)
+						{
+							newEventCommands[i] = farmerFinalPose;
+						}
+
+						else
+						{
+							newEventCommands[i] = eventCommands[i - 1];
+						}
+					}
+
+					__instance.eventCommands = newEventCommands;
+				}
+			}
+
+			catch(Exception e)
+			{
+				Monitor.Log($"Failed in {nameof(EventPatched)}:\n{e}", LogLevel.Error);
+			}
+		}
+
+		private static string fixFarmerDance(string curr)
+		{
+			curr = fixWarps(curr);
+			curr = fixAnimates(curr);
+
+			return curr;
+		}
+
+		private static string fixWarps(string curr)
+		{
+			if(curr.Contains("warp"))
+			{
+				curr = curr.Replace("21", "24"); // Move Girl Pos Down
+			}
+
+			return curr;
+		}
+
+		private static string fixAnimates(string curr)
+		{
+			if(!curr.Contains("animate"))
+			{
+				return curr;
+			}
+
+			string farmerName = "";
+			string[] words = curr.Split(' ');
+
+			foreach (string word in words)
+			{
+				if (word.Contains("farmer"))
+				{
+					farmerName = word;
+					break;
+				}
+			}
+
+			if(curr.Contains("animate " + farmerName + " false true 596 4 0"))
+			{
+				isMalePosition = false;
+			}
+
+			if (!isMalePosition)
+			{
+				curr = curr.Replace("animate " + farmerName + " false true 596 4 0", "animate " + farmerName + " false true 596 95 67 95 70"); // Phase 1 - Animate Girl
+				curr = curr.Replace("animate " + farmerName + " false true 600 0 3", "animate " + farmerName + " false true 600 96 100"); // Phase 2 - Animate Girl
+			}
+
+			else
+			{
+				curr = curr.Replace("animate " + farmerName + " false true 600 12 13 12 14", "animate " + farmerName + " false true 600 12 33"); // Phase 1 - Animate Guy
+				curr = curr.Replace("animate " + farmerName + " false true 150 12 13 12 14", "animate " + farmerName + " false true 300 13 14"); // Phase 2 - Animate Guy
+			}
+
+			return curr;
+		}
+	}
+}
